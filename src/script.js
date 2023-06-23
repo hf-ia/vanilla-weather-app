3ed553ea9810ea38ca869469167f56b80daaa1d3
hasna <68031193+hf-ia@users.noreply.github.com>
2023-06-23 17:47:27 +0200

    updated the html with ids and js by having the real temperature displayed

--- src/script.js
+++ src/script.js
@@ -1,14 +1,20 @@
 function displayTemperature(response) {
-  console.log(response.data.condition);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "bd76e8fab1a2d30oe70ff54t488914c6";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+
